93461b835e4c35b1acefa0c5da0e072b0748a6c1
Alca <dyd0927@gmail.com>
2025-05-07 13:03:22 +0900

    테스트

---
+++ StageLink-1/src/main/java/com/pro/stagelink/StageLink1Application.java
@@ -0,0 +1,13 @@
+package com.pro.stagelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageLink1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageLink1Application.class, args);
+	}
+
+}
---
+++ StageLink-1/src/test/java/com/pro/stagelink/StageLink1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.stagelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageLink1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
